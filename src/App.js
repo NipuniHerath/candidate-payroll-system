4be9ceeda0ea77732b71b94fd4c322e4f7157b6e
DhananjaliHerath <nipunikaherath9@gmail.com>
2024-11-24 17:49:53 +0530

    Tailwind css set up2

--- src/App.js
+++ src/App.js
@@ -17,9 +17,7 @@ function App() {
         >
           Learn React and tailiwnd 
         </a>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     
       </header>
     </div>
   );
