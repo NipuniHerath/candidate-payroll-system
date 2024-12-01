6802c972837d65e81587a5ec36c2c12a5faa83b4
DhananjaliHerath <nipunikaherath9@gmail.com>
2024-12-01 23:58:23 +0530

    payments button update

--- src/components/Shared/Buttons.js
+++ src/components/Shared/Buttons.js
@@ -15,6 +15,7 @@ const Button = ({
   const variants = {
     //Payment buttons
     paymentwithVisa:"text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-800 dark:bg-white dark:border-gray-700 dark:text-gray-900 dark:hover:bg-gray-200 me-2 mb-2",
+    applePay:"text-white bg-[#050708] hover:bg-[#050708]/80 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#050708]/40 dark:focus:ring-gray-600 me-2 mb-2",
 
     //Button sizes
     extrasmall:"px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800",
